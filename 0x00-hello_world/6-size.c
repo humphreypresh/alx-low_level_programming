0f18fec1fcfffda2d744e30ed9dee22d82add783
humphreypresh <humphreyprecious2@gmail.com>
2022-09-08 15:18:10 -0700

    C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,9 +7,7 @@
  * Return: Always 0 (Successful)
  *
  */
- 
 int main(void)
-
 {
 	/* sizeof evaluates the size of a variable */
 
@@ -20,6 +17,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
-
-
 }
