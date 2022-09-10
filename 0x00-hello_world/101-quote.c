7db08c5d056f5797c6835d04dbb0f182cef414bd
humphreypresh <humphreyprecious2@gmail.com>
2022-09-10 15:41:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
- * Description: A code with a wrong return
- * Return: 0
+ * Description: A code that prints a text
+ * Return: 1
  */
 
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
