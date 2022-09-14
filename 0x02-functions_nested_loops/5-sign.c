b1c9c32ed7207d5c96f5f77c474daf1123ddb65c
humphreypresh <humphreyprecious2@gmail.com>
2022-09-13 19:17:03 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  */
 int print_sign(int n)
 {
-	if (n > 0);
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
@@ -16,14 +16,12 @@ int print_sign(int n)
 	else if (n <= 0)
 	{
 		_putchar('-');
-
 		return (-1);
-		
 	}
+
 	else
+
 	{
 		_putchar('0');
-
-		return (0);
-
+		return (-1);
 	}}
