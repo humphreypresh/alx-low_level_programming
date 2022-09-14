a680e40854fbbebd44a0193241a856334938e162
humphreypresh <humphreyprecious2@gmail.com>
2022-09-14 08:33:38 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,13 +4,11 @@
  * @n: takes number input
  */
 void print_times_table(int n)
-
 {
 	int prod, mult, num;
 
 	if (n <= 15 && n >= 0)
 	{
-
 		for (num = 0; num <= n; ++num)
 		{
 
@@ -20,29 +18,22 @@ void print_times_table(int n)
 			{
 
 				_putchar(',');
-
 				_putchar(' ');
 
 				prod = num * mult;
 
 				if (prod <= 9)
-
 					_putchar(' ');
 
 				if (prod <= 99)
-
 					_putchar(' ');
 
 				if (prod >= 100)
 				{
-
 					_putchar((prod / 100) + 48);
-
 					_putchar((prod / 10) % 10 + 48);
 
-				}
-
-				else if (prod <= 99 && prod >= 10)
+				} else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
 			}
