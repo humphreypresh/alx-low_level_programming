cd4fce68e0ff2631856f4eb1384c28f7753ce5c4
humphreypresh <humphreyprecious2@gmail.com>
2022-09-15 15:40:28 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,6 +5,7 @@
  *  @month: month in number format
  *  @day: day of month
  *  @year: year
+ *  Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
@@ -14,9 +15,9 @@ void print_remaining_days(int month, int day, int year)
 	 * by 100 and 400 or by 4 evenly
 	 */
 	{
-		if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+		if ((year % 4 == 0) && (year % 400 == 0 || year % 100 !== 0))
 		{
-			if (month > 2 && day >= 60)
+			if (month >= 3 && day >= 60)
 			{
 				day++;
 			}
