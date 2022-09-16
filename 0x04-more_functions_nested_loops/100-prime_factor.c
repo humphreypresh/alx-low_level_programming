b3c7992ee37ce925bc44c84326f528f5068c5381
humphreypresh <humphreyprecious2@gmail.com>
2022-09-16 13:17:41 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,31 +1,69 @@
-#include"main.h"
+#include<stdio.h>
 
 /**
- * print_triangle - prints a triangle
+ *  _sqrt - finds the square root
  *
- * @size: size of the triangle
+ *   @x: input number
  *
- *  Return: Always 0 (Success) 
+ *   Return: square root of x
+ *
  */
 
-void print_triangle(int size)
+double _sqrt(double x)
 {
-	int hght, base;
+	float sqrt, tmp;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+	sqrt = x / 2;
+	tmp = 0;
+
+	while (sqrt != tmp)
 	{
-		for (hght = 1; hght <= size; ++hght)
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+	return (sqrt);
+}
+
+/**
+ * largest_prime_factor - finds and prints the largest
+ * prime factor of number (num)
+ *
+ * @num: number to find
+ */
+
+void largest_prime_factor(long int num)
+{
+	int prmNu, largest
+
+		/* first divide with the smallest prime number (two) */
+		while (num % 2 == 0)
+			num = num / 2;
+
+	/* num must be odd so we proceed to the next prime number (plus two) */
+	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
+	{
+		while (num % prmNu == 0)
 		{
-			for (base = 1; base <= size; ++base)
-			{
-				if ((hght + base) <= size)
-					_putchar(' ');
-				else
-					_putchar('#');
-			}
-			_putchar('\n');
+			num = num / prmNu;
+			largest = prmNu;
 		}
 	}
+
+
+	if (num > 2)
+		largest = num;
+	printf("%d\n", largest);
+}
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	largest_prime_factor(612852475143);
+
+	return (0);
 }
