5abb3e8c53811b9aaa0aa55224ca99600252e622
humphreypresh <humphreyprecious2@gmail.com>
2022-09-29 09:05:03 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _factorial - a function that returns the 
+ * _factorial - a function that returns the
  * factorial of *n
  *
- * @n: positive numbe
+ * @n: positive number
  *
  * Return: factorial of @n
  */
 
 int factorial(int n)
-{  
+{
 	if (n < 0)            /* error -1 if number is negative */
 		return (-1);
 	else if  (n == 0)    /* return factorial of 0 = 1 */
